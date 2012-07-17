28a04c9858cd05bc9a8d861b01e157fb2e7280e1
Tyler Menezes <tylermenezes@gmail.com>
2012-07-17 21:50:39 +0000

    Add simple require support to base controller

--- CuteControllers/Base/Controller.php
+++ CuteControllers/Base/Controller.php
@@ -11,5 +11,35 @@ abstract class Controller
         $this->request = $request;
     }
 
+    protected function require_get()
+    {
+        $required = func_get_args();
+        foreach ($required as $require) {
+            if ($this->request->get($require) === NULL) {
+                throw new \CuteControllers\HttpError(400);
+            }
+        }
+    }
+
+    protected function require_post()
+    {
+        $required = func_get_args();
+        foreach ($required as $require) {
+            if ($this->request->post($require) === NULL) {
+                throw new \CuteControllers\HttpError(400);
+            }
+        }
+    }
+
+    protected function require_request()
+    {
+        $required = func_get_args();
+        foreach ($required as $require) {
+            if ($this->request->request($require) === NULL) {
+                throw new \CuteControllers\HttpError(400);
+            }
+        }
+    }
+
     abstract public function route();
 }
